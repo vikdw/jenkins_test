5fae8e6fe4e072c623634a365446b67bf2a11bc0
vikdw <vikasdwivedi172@yahoo.com>
2025-01-26 11:30:16 +0530

    adding 2 groovy scriot for tesing in sharelibrary testing.

--- vars/1.groovy
+++ vars/1.groovy
@@ -1,1 +1,4 @@
-test
+def func_from_module_1(){
+    println("This is a test function from module 1 and function func_from_module_1")
+    println("This function can be used in any pipeline stages")
+}
