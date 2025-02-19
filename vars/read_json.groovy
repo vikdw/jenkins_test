f004e04e07164f4b0994276ccb224ef44d15b280
vikdw <vikasdwivedi172@yahoo.com>
2025-02-19 12:37:59 +0530

    update json.groovy

--- vars/read_json.groovy
+++ vars/read_json.groovy
@@ -1,4 +1,12 @@
+import groovy.json.JsonSlurper
+
+def jsonSlurper = new JsonSlurper()
+
+def config = jsonSlurper.parse(new File('config.json'))
+
+println "config = $config"
+
 def read_json(json_file){
-    def props = readJSON file: json_file
-    return props 
+    def config = jsonSlurper.parse(new File(json_file))
+    return config 
 }
