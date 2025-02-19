262a4d5f2ec1a0a439cf71c13d1357bd3ecf344d
vikdw <vikasdwivedi172@yahoo.com>
2025-02-19 12:41:52 +0530

    update read_json.groovy

--- vars/read_json.groovy
+++ vars/read_json.groovy
@@ -3,7 +3,7 @@ import groovy.json.JsonSlurper
 
 def read_json(json_file){
     def jsonSlurper = new JsonSlurper()
-    def config = jsonSlurper.parse(new File('config.json'))
+    
     def config = jsonSlurper.parse(new File(json_file))
     return config 
 }
