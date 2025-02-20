e1a3e93c5b99cf87cbbbcd4f55f521675ddfe405
vikdw <vikasdwivedi172@yahoo.com>
2025-02-20 11:54:56 +0530

    modify json reader for file variale.

--- vars/read_json.groovy
+++ vars/read_json.groovy
@@ -1,8 +1,6 @@
 def jsonReader(file){
     def scriptcontents = libraryResource "${file}"  
-    writeFile file: "temp_data.json", text: scriptcontents 
+    writeFile file: "${file}", text: scriptcontents 
     sh "ls -lrt"
-    sh "cat temp_data.json"
-
-
+    sh "cat ${file}"
 }
