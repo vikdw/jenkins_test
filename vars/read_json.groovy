82b6f53a06bcbd27af91a95fb35dc16d41d43eb3
vikdw <vikasdwivedi172@yahoo.com>
2025-02-20 11:46:13 +0530

    modified /var/jsonreader to read file in temp.

--- vars/read_json.groovy
+++ vars/read_json.groovy
@@ -1,6 +1,6 @@
 def jsonReader(){
-    def tempFile = libraryResource "data.json"
-    writeFile file : "temp_data.json" tempFile
+    def scriptcontents = libraryResource "data.json"  
+    writeFile file: "temp_data.json", text: scriptcontents 
     sh "ls -lrt"
     sh "cat temp_data.json"
 
